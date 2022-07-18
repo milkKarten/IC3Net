4ad17374764b3b8bfc33ce1d6efd904040ca153b
Stephane Hatgis-Kessell <stephanehk@ava.ri.cmu.edu>
2022-07-17 22:49:44 -0400

    VQ-VAE implementation

--- networks.py
+++ networks.py
@@ -7,6 +7,56 @@ import numpy as np
 from network_utils import gumbel_softmax, onehot_from_logits
 
 
+
+class VectorQuantizer(nn.Module):
+    """
+    Reference:
+    [1] https://github.com/deepmind/sonnet/blob/v2/sonnet/src/nets/vqvae.py
+    [2] https://github.com/AntixK/PyTorch-VAE/blob/master/models/vq_vae.py
+    """
+    def __init__(self,num_embeddings,embedding_dim,beta = 0.25):
+        super(VectorQuantizer, self).__init__()
+        self.K = num_embeddings
+        self.D = embedding_dim
+        self.beta = beta
+
+        self.embedding = nn.Embedding(self.K, self.D)
+        self.embedding.weight.data.uniform_(-1 / self.K, 1 / self.K)
+
+    def forward(self, latents):
+        #Latents have shape [B X N_AGENTS X D]
+        latents_shape = latents.shape
+        flat_latents = latents.view(-1, self.D)  # [N_AGENTS x D]
+
+        # Compute L2 distance between latents and embedding weights
+        dist = torch.sum(flat_latents ** 2, dim=1, keepdim=True) + \
+               torch.sum(self.embedding.weight ** 2, dim=1) - \
+               2 * torch.matmul(flat_latents, self.embedding.weight.t())  # [N_AGENTS x K]
+
+        # Get the encoding that has the min distance
+        encoding_inds = torch.argmin(dist, dim=1).unsqueeze(1)  # [N_AGENTS, 1]
+
+        # Convert to one-hot encodings
+        device = latents.device
+        encoding_one_hot = torch.zeros(encoding_inds.size(0), self.K, device=device)
+        encoding_one_hot.scatter_(1, encoding_inds, 1)  # [N_AGENTS x K]
+
+        # Quantize the latents
+        quantized_latents = torch.matmul(encoding_one_hot, self.embedding.weight)  # [N_AGENTS, D]
+        quantized_latents = quantized_latents.view(latents_shape)  # [N_AGENTS x D]
+
+        # Compute the VQ Losses
+        commitment_loss = F.mse_loss(quantized_latents.detach(), latents)
+        embedding_loss = F.mse_loss(quantized_latents, latents.detach())
+
+        vq_loss = commitment_loss * self.beta + embedding_loss
+
+        # Add the residue back to the latents
+        quantized_latents = latents + (quantized_latents - latents).detach()
+
+        return quantized_latents, vq_loss  
+
+
 class ProtoLayer(nn.Module):
     def __init__(self, comm_dim, num_prototypes):
         super(ProtoLayer, self).__init__()
